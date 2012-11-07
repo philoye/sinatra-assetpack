94e1bdd771d155e0a90b72331178bf3cf081a9f6
Phil Oye <philoye@philoye.com>
2012-11-07 11:11:16 +1100

    Check which file on disk matches requested file (css could mean css/sass/less, etc.)

--- lib/sinatra/assetpack/options.rb
+++ lib/sinatra/assetpack/options.rb
@@ -237,22 +237,28 @@ module Sinatra
       # Returns the local file for a given URI path. (for dynamic files)
       # Returns nil if a file is not found.
       # TODO: consolidate with local_file_for
-      def dyn_local_file_for(requested_file, from)
-        file = requested_file
+      def dyn_local_file_for(file, from)
         extension = ''
         file.sub(/^(.*)\.([^\.]+)$/) { file, extension = $1, $2 }
 
         # Remove cache-buster (/js/app.28389.js => /js/app)
         file = $1 if file =~ /^(.*)\.[0-9]+$/
 
-        # Convert extension to its dynamic equivalent, e.g., js => coffee
-        # TODO: 'css' always returns sass, not sure how to get less
-        formats = Sinatra::AssetPack.tilt_formats
-        if formats.key( extension )
-          extension = formats.key( extension )
+        ## Find matching file (css file might represent css/sass/less/etc.)
+        # Get the list of possible preprocessor extensions (sass, less, etc.)
+        formats = Sinatra::AssetPack.tilt_formats.reject{|k,v|v != extension}.keys
+        # Let's add the original extension first
+        formats.unshift extension
+        # Let's see which exists
+        file_on_disk = nil
+        formats.each do |fmt|
+          test_file = File.join(app.root, from, "#{file}.#{fmt}")
+          if File.exist?(test_file)
+            file_on_disk = test_file
+            break
+          end
         end
-
-        Dir[File.join(app.root, from, "#{file}.#{extension}")].first
+        return file_on_disk
       end
 
       # Writes `public/#{path}` based on contents of `output`.
